e9f92949a59ea88be5d8b01d8de4dcb3e4e80f27
Rick Martin <rmartin1985@att.net>
2021-10-26 18:22:24 -0500

    CRUD for users in the db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
-// import the Sequelize constructor from the Library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our database
+// create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
 });
 
 module.exports = sequelize;
