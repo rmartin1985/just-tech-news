7710e5595729b51f4fb2742d3ec1010b8e610cd4
Rick Martin <rmartin1985@att.net>
2021-11-04 18:25:25 -0500

    partials added

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
+const helpers = require('./utils/helpers')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const SequleizeStore = require('connect-session-sequelize')(session.Store);
 
